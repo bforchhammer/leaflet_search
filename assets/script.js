9cff2ae6c9146e7345dd9251c922240af72e6b7d
Benedikt Forchhammer <b.forchhammer@gmail.com>
2014-11-12 18:57:41 +0100

    Added topcenter region. Added option to avoid adding icons. Fixed styling.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,59 @@
 (function ($) {
+
+    // Fix bug where ESC key collapses search field, even if it is not collapsible.
+    var _keydown_callback = L.Control.Geocoder.prototype._keydown;
+    L.Control.Geocoder.prototype._keydown = function (e) {
+        if (e.keyCode == 27 && !this.options.collapsed) {
+            // Ignore ESC keypress if the widget is not collapsible.
+            return;
+        }
+        return _keydown_callback.apply(this, arguments);
+    };
+
+    // Add option to not have a marker placed for the selected result.
+    var markGeocode_callback = L.Control.Geocoder.prototype.markGeocode;
+    L.Control.Geocoder.prototype.markGeocode = function (result) {
+        if (this.options.noResultIcon) {
+            // If we don't want a result icon, only fit bounds and remove any previous icons.
+            this._map.fitBounds(result.bbox);
+            if (this._geocodeMarker) {
+                this._map.removeLayer(this._geocodeMarker);
+            }
+            return this;
+        }
+        // Else, execute default behavior
+        return markGeocode_callback.apply(this, arguments);
+    };
+
     $(document).on('leaflet.map', function (e, settings, lMap) {
         if (settings.plugins.search) {
-            var geocoder = L.Control.geocoder(settings.plugins.search).addTo(lMap);
+            var geocoder_settings = settings.plugins.search;
+
+            // 1) Initialise a "topcenter" control container.
+            if (geocoder_settings.position == 'topcenter') {
+                var $controlContainer = lMap._controlContainer,
+                    nodes = $controlContainer.childNodes,
+                    topCenter = false;
+
+                for (var i = 0, len = nodes.length; i < len; i++) {
+                    var klass = nodes[i].className;
+                    if (/leaflet-top/.test(klass) && /leaflet-center/.test(klass)) {
+                        topCenter = true;
+                        break;
+                    }
+                }
+                if (!topCenter) {
+                    var tc = document.createElement('div');
+                    tc.className += 'leaflet-top leaflet-center';
+                    $controlContainer.appendChild(tc);
+                    lMap._controlCorners.topcenter = tc;
+                }
+            }
+
+            // 2) Create and attach geocoder control.
+            var geocoder = L.Control.geocoder(geocoder_settings);
+            geocoder.addTo(lMap);
+
         }
     });
 })(jQuery);
